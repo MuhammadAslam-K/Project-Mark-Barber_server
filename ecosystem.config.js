944d949d1cb6ca7a6749e831ec0bbf4cde608186
Muhammad Aslam K A <112756291+MuhammadAslam-K@users.noreply.github.com>
2023-11-13 19:11:15 +0530

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,20 @@
 module.exports = {
-    apps: [
-      {
-        name: 'your-app',
-        script: 'dist/adapters/index.js',
-        interpreter: 'ts-node',
-        env: {
-          NODE_ENV: 'production',
-          PORT=8000
-          FRONT_END="https://markbarber.in/"
-          MONGO_URL="mongodb+srv://markbarbercoin:markbarber@cluster0.cmxse7y.mongodb.net/markbarber?retryWrites=true&w=majority"
-          ADMIN_EMAIL=markbarber.co.in@gmail.com
-          ADMIN_PASSWORD=Markbarber1998
-          SECRET_KEY=JkChTjrw8N4z2D83h3geiNM7qfRtcZRU0isSgNgq          
-        },
-        exec_mode: 'fork', // or 'cluster'
-        instances: 1, // Adjust as needed based on your server capacity
+  apps: [
+    {
+      name: 'your-app',
+      script: 'dist/adapters/index.js',
+      interpreter: 'ts-node',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 8000,
+        FRONT_END: "https://markbarber.in/",
+        MONGO_URL: "mongodb+srv://markbarbercoin:markbarber@cluster0.cmxse7y.mongodb.net/markbarber?retryWrites=true&w=majority",
+        ADMIN_EMAIL: "markbarber.co.in@gmail.com",
+        ADMIN_PASSWORD: "Markbarber1998",
+        SECRET_KEY: "JkChTjrw8N4z2D83h3geiNM7qfRtcZRU0isSgNgq",
       },
-    ],
-  };
-  
+      exec_mode: 'fork',
+      instances: 1
+    },
+  ],
+};
